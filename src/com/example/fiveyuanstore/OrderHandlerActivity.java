1fd2c77c6316fd07dca5300e9896308491852443
LWJCoder <15818464582@163.com>
2016-12-19 19:38:35 +0800

    oder handler

---
+++ src/com/example/fiveyuanstore/OrderHandlerActivity.java
@@ -0,0 +1,14 @@
+package com.example.fiveyuanstore;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderHandlerActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_oder_handler);
+	}
+}
