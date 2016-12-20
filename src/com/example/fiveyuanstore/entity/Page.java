354ee123872887104cdae17c0817914c8bf2a0d1
LWJCoder <15818464582@163.com>
2016-12-20 19:48:01 +0800

    register login

---
+++ src/com/example/fiveyuanstore/entity/Page.java
@@ -0,0 +1,35 @@
+package com.example.fiveyuanstore.entity;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Page <T>{
+	List <T> content;
+	Integer number;
+	
+	public List <T> getContent() {
+		return content;
+	}
+	public void setContent(List <T> content) {
+		this.content = content;
+	}
+	public Integer getNumber() {
+		return number;
+	}
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	public Goods get(int position) {
+		return (Goods) content.get(position);
+	}
+	public int size() {
+		return content.size();
+	}
+	public void addAll(List<T> content2) {
+		setContent(content2);
+		
+	}
+	
+}
