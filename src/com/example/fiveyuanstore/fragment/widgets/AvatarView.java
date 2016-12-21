7aa381da9ab094ae3ced874db3ac5a913a1f36fc
LWJCoder <15818464582@163.com>
2016-12-21 18:50:36 +0800

    avatar

--- src/com/example/fiveyuanstore/fragment/widgets/AvatarView.java
+++ src/com/example/fiveyuanstore/fragment/widgets/AvatarView.java
@@ -2,7 +2,8 @@ package com.example.fiveyuanstore.fragment.widgets;
 
 import java.io.IOException;
 
-
+import com.example.fiveyuanstore.api.Server;
+import com.example.fiveyuanstore.entity.User;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -13,8 +14,15 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Shader.TileMode;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class AvatarView extends View {				//头像处理
@@ -33,72 +41,75 @@ public class AvatarView extends View {				//头像处理
 	
 	Paint paint;
 	float srcWidth, srcHeight;
+	Handler mainThreadHandler = new Handler();
 	
 	public void setBitmap(Bitmap bmp){					//确认头像内容
 		if(bmp==null) {
 			paint = new Paint();
 			paint.setColor(Color.GRAY);
 			paint.setStyle(Paint.Style.STROKE);
-			paint.setStrokeWidth(1);
+			paint.setStrokeWidth(10);
 		    paint.setPathEffect(new DashPathEffect(new float[]{5, 10, 15, 20}, 0));
 			paint.setAntiAlias(true);
+			Log.d("paint123", "1111");
 		}else{
 			paint = new Paint();
 			paint.setShader(new BitmapShader(bmp, TileMode.REPEAT, TileMode.REPEAT));
 			paint.setAntiAlias(true);
 			
 			srcWidth = bmp.getWidth();
-			srcHeight = bmp.getHeight();	
+			srcHeight = bmp.getHeight();
+			Log.d("paint123", "2222");
 		}
 		invalidate();
 	}
 																//从service获取头像数据
-//	public void load(User user){				
-//		load(Server.serverAddress + user.getAvatar());
-//	}
+	public void load(User user){				
+		load(Server.serverAddress + user.getAvatar());
+	}
 	
-	public void load(/*String url*/){
+	public void load(String url){
+						
+		OkHttpClient client = Server.getClient();
 		
-		setBitmap(null);
+		Request request = new Request.Builder()
+				.url(url)
+				.method("get", null)
+				.build();
 		
-				
-//		OkHttpClient client = Server.getSharedClient();
-//		
-//		Request request = new Request.Builder()
-//				.url(url)
-//				.method("get", null)
-//				.build();
-//		
-//		client.newCall(request).enqueue(new Callback() {
-//			
-//			@Override
-//			public void onResponse(Call arg0, Response arg1) throws IOException {
-//				try{
-//					byte[] bytes = arg1.body().bytes();
-//					final Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-//					mainThreadHandler.post(new Runnable() {
-//						public void run() {
-//							setBitmap(bmp);
-//						}
-//					});
-//				}catch(Exception ex){
-//					mainThreadHandler.post(new Runnable() {
-//						public void run() {
-//							setBitmap(null);
-//						}
-//					});
-//				}
-//			}
-//			
-//			@Override
-//			public void onFailure(Call arg0, IOException arg1) {
-//				mainThreadHandler.post(new Runnable() {
-//					public void run() {
-//						setBitmap(null);
-//					}
-//				});
-//			}
-//		});
+		client.newCall(request).enqueue(new Callback() {
+			
+			@Override
+			public void onResponse(Call arg0, Response arg1) throws IOException {
+				try{
+					byte[] bytes = arg1.body().bytes();
+					final Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+					mainThreadHandler.post(new Runnable() {
+						public void run() {
+							Log.d("paint123", "3333");
+							setBitmap(bmp);
+						}
+					});
+				}catch(Exception ex){
+					mainThreadHandler.post(new Runnable() {
+						public void run() {
+							Log.d("paint123", "4444");
+							setBitmap(null);
+						}
+					});
+				}
+			}
+			
+			@Override
+			public void onFailure(Call arg0, IOException arg1) {
+				mainThreadHandler.post(new Runnable() {
+					public void run() {
+						Log.d("paint123", "5555");
+						setBitmap(null);
+					}
+				});
+			}
+		});
 	}
 
 	@Override
