404bd917433477eb1b7c83eabbb6d8f64cc5ec1f
Administrator <Administrator@7A304-0039>
2016-12-26 17:03:02 +0800

    ...

--- src/com/example/fiveyuanstore/api/Server.java
+++ src/com/example/fiveyuanstore/api/Server.java
@@ -18,7 +18,7 @@ public class Server {
 	static OkHttpClient client;
 
 
-	public static String serverAddress = "http://172.27.15.18:8080/storecenter/";
+	public static String serverAddress = "http://172.27.15.25:8080/storecenter/";
 
 
 	
