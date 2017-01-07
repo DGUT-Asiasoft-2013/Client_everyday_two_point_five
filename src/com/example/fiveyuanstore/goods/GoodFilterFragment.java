e4ffb75e1821c5a22a96056bc3c74f3529aa16c6
Administrator <Administrator@7A304-0039>
2017-01-07 10:15:16 +0800

    修复筛选bug——不能为空问题，待解决问题：重复筛选会在已筛选基础上继续筛选

--- src/com/example/fiveyuanstore/goods/GoodFilterFragment.java
+++ src/com/example/fiveyuanstore/goods/GoodFilterFragment.java
@@ -16,23 +16,30 @@ public class GoodFilterFragment extends Fragment {
 	View view;
 	EditText text1;
 	EditText text2;
-	float flo1=0;
-	float flo2=0;
+	float flo1 = 0;
+	float flo2 = 0;
 	Boolean isConfirm;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		if (view == null) {
 			view = inflater.inflate(R.layout.fragment_goods_filter, null);
-			text1=(EditText)view.findViewById(R.id.editText1);
-			text2=(EditText)view.findViewById(R.id.editText2);
+			text1 = (EditText) view.findViewById(R.id.editText1);
+			text2 = (EditText) view.findViewById(R.id.editText2);
 		}
 		view.findViewById(R.id.layout_out).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				text1.setText(String.valueOf(flo1));
-				text2.setText(String.valueOf(flo2));
+				if(flo1!=0)
+					text1.setText(String.valueOf(flo1));
+				else
+					text1.setText("");
+				if(flo2!=Float.MAX_VALUE)
+					text2.setText(String.valueOf(flo2));
+				else
+					text2.setText("");
 				getActivity().onBackPressed();
 			}
 		});
@@ -47,61 +54,78 @@ public class GoodFilterFragment extends Fragment {
 
 			@Override
 			public void onClick(View v) {
-				float f1= Float.parseFloat(text1.getText().toString());;
-				float f2= Float.parseFloat(text2.getText().toString());;
-				if (f2 >= f1){
+				float f1=0,f2=0;
+				if(text1.getText().toString().equals("")||text1.getText()==null)
+					f1=0;
+					 
+				else
+					f1 = Float.parseFloat(text1.getText().toString());
+				if(text2.getText().toString().equals("")||text2.getText()==null)
+					f2=Float.MAX_VALUE;
+				else
+					
+				f2 = Float.parseFloat(text2.getText().toString());
+				if (f2 == 0) {
+					getActivity().runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							Toast.makeText(getActivity(), "最高价不能为0哦", Toast.LENGTH_SHORT).show();
+
+						}
+					});
+				} else if (f2 >= f1) {
 					if (OnFilterConfirmClickedListener != null) {
-						isConfirm=true;
-						flo1=f1;
-						flo2 =f2;
+						isConfirm = true;
+						flo1 = f1;
+						flo2 = f2;
 						OnFilterConfirmClickedListener.onFilterConfirmClicked();
 						getActivity().onBackPressed();
-					} 
-				}else
+					}
+				} else
 					getActivity().runOnUiThread(new Runnable() {
-						
+
 						@Override
 						public void run() {
 							Toast.makeText(getActivity(), "输入的价格必须是从低到高噢~", Toast.LENGTH_SHORT).show();
-							
+
 						}
 					});
 			}
-	
+
 		});
-			
+
 		view.findViewById(R.id.btn_cancle).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
 				if (OnFilterConfirmClickedListener != null) {
-					isConfirm=false;
-					flo1=flo2=0;
+					isConfirm = false;
+					flo1 = flo2 = 0;
 					text1.setText(String.valueOf(flo1));
 					text2.setText(String.valueOf(flo2));
 					OnFilterConfirmClickedListener.onFilterConfirmClicked();
 					getActivity().onBackPressed();
-				} 
+				}
 			}
 		});
-			
-			
-		
+
 		return view;
 	}
-	
-	public boolean isConfirm(){
+
+	public boolean isConfirm() {
 		return isConfirm;
 	}
-	public float getText1(){
+
+	public float getText1() {
 		return flo1;
 	}
-	
-	public float getText2(){
+
+	public float getText2() {
 		return flo2;
 	}
-	
+
 	public static interface OnFilterConfirmClickedListener {
 		void onFilterConfirmClicked();
 	}
