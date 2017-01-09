f22d4d7be2e016e23c26727968a10aa735c600e1
KevinJJ616 <616535909@qq.com>
2017-01-09 11:46:29 +0800

    购买界面，零钱界面

--- src/com/example/fiveyuanstore/TransactionActivity.java
+++ src/com/example/fiveyuanstore/TransactionActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -38,6 +39,15 @@ public class TransactionActivity extends Activity {
 
 		ListView listView = (ListView) findViewById(R.id.transaction_list);
 		listView.setAdapter(listAdapter);
+		
+		findViewById(R.id.btn_transaction_back).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();		
+			}
+		});
+		
 	}
 
 	BaseAdapter listAdapter = new BaseAdapter() {
