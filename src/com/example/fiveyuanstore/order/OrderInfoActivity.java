9544f6a0ebea9137d35d0e8ab8e117ca1126740a
DissoFly <280596129@qq.com>
2017-01-10 15:17:24 +0800

    修改订单详情ui,添加卖家头像和用户名，MyOrder里增加buyer_id

--- src/com/example/fiveyuanstore/order/OrderInfoActivity.java
+++ src/com/example/fiveyuanstore/order/OrderInfoActivity.java
@@ -3,36 +3,48 @@ package com.example.fiveyuanstore.order;
 import java.io.IOException;
 
 import com.example.fiveyuanstore.R;
+import com.example.fiveyuanstore.ZoneActivity;
 import com.example.fiveyuanstore.R.id;
 import com.example.fiveyuanstore.R.layout;
 import com.example.fiveyuanstore.api.Server;
 import com.example.fiveyuanstore.customViews.ProImgView;
 import com.example.fiveyuanstore.entity.MyOrder;
+import com.example.fiveyuanstore.entity.User;
+import com.example.fiveyuanstore.fragment.widgets.AvatarView;
+import com.example.fiveyuanstore.page.MyProfileFragment;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OrderInfoActivity extends Activity {
 	MyOrder order;
+	User buyerUser;
 	int position = 0;
 	int state = 0;
-
+	TextView buyerNameInUser;
+	AvatarView avatar;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_order_info);
 		order = (MyOrder) getIntent().getSerializableExtra("orders");
+		buyerUser=order.getUser();
 		TextView sureSendGoods = (TextView) findViewById(R.id.sureSendGoods);
 		TextView cancleOrder = (TextView) findViewById(R.id.cancleOrder);
 		ProImgView proImg = (ProImgView) findViewById(R.id.proImg);
@@ -45,10 +57,20 @@ public class OrderInfoActivity extends Activity {
 		TextView buyerName = (TextView) findViewById(R.id.name);
 		TextView phone = (TextView) findViewById(R.id.phone);
 		TextView adress = (TextView) findViewById(R.id.adress);
+		buyerNameInUser=(TextView) findViewById(R.id.buyer_name);
+		avatar=(AvatarView) findViewById(R.id.avatar);
+		
 		proImg.load(order.getGoods());
 		orderId.setText("订单编号： " + order.getOrder_num());
 		name.setText(" " + order.getGoods().getText());
-
+		findViewById(R.id.btn_order_info_back).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
 		try {
 			String dateStr = DateFormat.format("yyyy-MM-dd hh:mm", order.getGoods().getCreateDate()).toString();
 			date.setText("" + dateStr);
@@ -103,6 +125,56 @@ public class OrderInfoActivity extends Activity {
 					cancleOrder();
 					state = 2;
 				}
+			}
+		});
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		reload();
+	}
+	
+	void reload(){
+		OkHttpClient client = Server.getClient();
+		Request request = Server.requestBuilderWithPath("/user/"+order.getBuyer_id()).get().build();
+
+		client.newCall(request).enqueue(new Callback() {
+
+			@Override
+			public void onResponse(final Call arg0, Response res) throws IOException {
+				try {
+					final User user = new ObjectMapper().readValue(res.body().string(),
+							new TypeReference<User>() {
+							});
+
+						runOnUiThread(new Runnable() {
+
+							public void run() {
+								OrderInfoActivity.this.buyerUser=user;
+								avatar.load(buyerUser);
+								buyerNameInUser.setText(buyerUser.getUser_name());
+								
+							}
+						});
+					
+				} catch (final Exception e) {
+					Toast.makeText(getApplicationContext(), e.getMessage(),
+							Toast.LENGTH_SHORT).show();
+				}
+				
+				
+			}
+			@Override
+			public void onFailure(final Call arg0, final IOException arg1) {
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						Toast.makeText(getApplicationContext(), arg1.getMessage(),
+								Toast.LENGTH_SHORT).show();
+					}
+				});
 			}
 		});
 	}
